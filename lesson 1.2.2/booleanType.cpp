81bc72e1a84f46e41aaeb8d120ea6821f9fef003
Vitaliy Klimov <vitaliy.inc.if.uk@gmail.com>
2021-01-17 22:26:31 +0200

    lesson 1.2.2

---
+++ lesson 1.2.2/booleanType.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char* argvp[]) {
+
+    bool goodCase = true;
+    bool badCase = false;
+    
+    bool convertion_1 = 1;
+    bool convertion_2 = 0;
+    bool convertion_3 = 0.12345;
+
+    std::cout << "convertion_1 = " << convertion_1 << std::endl;
+    std::cout << "convertion_2 = " << convertion_2 << std::endl;
+    std::cout << "convertion_3 = " << convertion_3 << std::endl;
+
+    return 0;
+}
